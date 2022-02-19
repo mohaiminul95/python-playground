83a44d342d700676be2248a6d118ec95d25ec8cd
mohaiminul95 <rabidislam94@gmail.com>
2022-02-19 17:53:06 +0600

    selenium updated

--- selenium/main.py
+++ selenium/main.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
+import json
 
 # driver config
 webdriver_path = "/media/rabid/Development/PYTHON/python-playground/selenium/chromedriver/chromedriver"
@@ -10,19 +15,28 @@ driver = webdriver.Chrome(service=service, options=options)
 driver.maximize_window()
 
 # site url
-driver.get('https://sims.technometrics.net')
+driver.get('https://www.techwithtim.net/')
 
-# admin access
-email = "admin@sims.net"
-password = 12345678
+search = driver.find_element(By.NAME, "s")
+search.send_keys('test')
+search.send_keys(Keys.RETURN)
 
-app_email = driver.find_element_by_id('login-email')
-app_password = driver.find_element_by_id('login-password')
-app_email.send_keys(email)
-app_password.send_keys(password)
+# GET CONTENTS
+posts = []
+try:
+    body = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "page"))
+    )
+    articles = body.find_elements(By.TAG_NAME, "article")
+    for article in articles:
+        post_time = article.find_element(By.CLASS_NAME, 'entry-meta').text
+        post_content = article.find_element(By.CLASS_NAME, 'entry-summary').text
+        posts.append({'time' : post_time, 'content' : post_content})  
+except:
+    driver.quit()
 
-loginBtn = driver.find_element_by_class_name('waves-light')
-loginBtn.click()
+result = {'count' : len(posts), 'posts' : posts}    
+print(json.dumps(result, indent=4))
 
-time.sleep(15)
+time.sleep(7)
 driver.close()
